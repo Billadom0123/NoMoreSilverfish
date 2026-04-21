71bfe937cde3d3cda487309d1793c9b600aaae28
Billadom0123 <3233405848@qq.com>
2026-04-21 22:52:39 +0800

    refactor: GO TO neoforge-1.21.1

--- src/main/java/com/billadom/nomoresilverfish/Nomoresilverfish.java
+++ src/main/java/com/billadom/nomoresilverfish/Nomoresilverfish.java
@@ -1,11 +1,13 @@
 package com.billadom.nomoresilverfish;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -16,13 +18,16 @@ public class Nomoresilverfish {
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public Nomoresilverfish() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+    public Nomoresilverfish(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
+        NeoForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("OMG I HATE SILVERFISH!");
     }
 }
