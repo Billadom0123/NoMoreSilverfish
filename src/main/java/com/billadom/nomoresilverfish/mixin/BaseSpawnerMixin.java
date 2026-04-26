a0301db378c7b3de9b676bc9878a5a34d09c86d5
Billadom0123 <3233405848@qq.com>
2026-04-26 23:27:30 +0800

    add: 已有的蠹虫刷怪笼不显示模型
    todo: 已有的也替换

--- src/main/java/com/billadom/nomoresilverfish/mixin/BaseSpawnerMixin.java
+++ src/main/java/com/billadom/nomoresilverfish/mixin/BaseSpawnerMixin.java
@@ -1,9 +1,12 @@
 package com.billadom.nomoresilverfish.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BaseSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BaseSpawner.class)
@@ -23,5 +26,30 @@ public class BaseSpawnerMixin {
     }
 //    public void setEntityId(EntityType<?> p_253682_, @Nullable Level p_254041_, RandomSource p_254221_, BlockPos p_254050_) {
 //        this.getOrCreateNextSpawnData(p_254041_, p_254221_, p_254050_).getEntityToSpawn().putString("id", BuiltInRegistries.ENTITY_TYPE.getKey(p_253682_).toString());
+//    }
+
+    @ModifyReturnValue(
+            method = "getOrCreateDisplayEntity",
+            at = @At("RETURN")
+    )
+    private Entity nomoresilverfish$modifyDisplayEntity(Entity original) {
+        if (original != null && original.getType() == EntityType.SILVERFISH) {
+            return null;
+        }
+        return original;
+    }
+//    public Entity getOrCreateDisplayEntity(Level level, BlockPos pos) {
+//        if (this.displayEntity == null) {
+//            CompoundTag compoundtag = this.getOrCreateNextSpawnData(level, level.getRandom(), pos).getEntityToSpawn();
+//            if (!compoundtag.contains("id", 8)) {
+//                return null;
+//            }
+//
+//            this.displayEntity = EntityType.loadEntityRecursive(compoundtag, level, Function.identity());
+//            if (compoundtag.size() == 1 && this.displayEntity instanceof Mob) {
+//            }
+//        }
+//
+//        return this.displayEntity;
 //    }
 }
---
+++ src/main/java/com/billadom/nomoresilverfish/mixin/SpawnerBlockEntityMixin.java
@@ -0,0 +1,13 @@
+package com.billadom.nomoresilverfish.mixin;
+
+import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpawnerBlockEntity.class)
+public class SpawnerBlockEntityMixin {
+}
