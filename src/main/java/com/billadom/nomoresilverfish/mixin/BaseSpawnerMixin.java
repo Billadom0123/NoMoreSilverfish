a75d0dd13b167eac8d52ff54df80829af63b3cbf
Billadom0123 <3233405848@qq.com>
2026-04-27 10:51:04 +0800

    add: 已有的蠹虫刷怪笼不显示模型
    todo: 已有的直接替换，或禁止生成

--- src/main/java/com/billadom/nomoresilverfish/mixin/BaseSpawnerMixin.java
+++ src/main/java/com/billadom/nomoresilverfish/mixin/BaseSpawnerMixin.java
@@ -1,10 +1,16 @@
 package com.billadom.nomoresilverfish.mixin;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BaseSpawner.class)
 public class BaseSpawnerMixin {
@@ -23,5 +29,33 @@ public class BaseSpawnerMixin {
     }
 //    public void setEntityId(EntityType<?> p_253682_, @Nullable Level p_254041_, RandomSource p_254221_, BlockPos p_254050_) {
 //        this.getOrCreateNextSpawnData(p_254041_, p_254221_, p_254050_).getEntityToSpawn().putString("id", BuiltInRegistries.ENTITY_TYPE.getKey(p_253682_).toString());
+//    }
+
+    @Inject(
+            method = "getOrCreateDisplayEntity",
+            at = @At("RETURN"),
+            cancellable = true
+    )
+    private void nomoresilverfish$modifyDisplayEntity(Level p_254323_, RandomSource p_254353_, BlockPos p_254313_, CallbackInfoReturnable<Entity> cir) {
+        Entity displayEntity = cir.getReturnValue();
+        if (displayEntity != null && displayEntity.getType() == EntityType.SILVERFISH) {
+            cir.setReturnValue(null);
+        }
+    }
+
+//    @Nullable
+//    public Entity getOrCreateDisplayEntity(Level level, BlockPos pos) {
+//        if (this.displayEntity == null) {
+//            CompoundTag compoundtag = this.getOrCreateNextSpawnData(level, level.getRandom(), pos).getEntityToSpawn();
+//            if (!compoundtag.contains("id", 8)) {
+//                return null;
+//            }
+//
+//            this.displayEntity = EntityType.loadEntityRecursive(compoundtag, level, Function.identity());
+//            if (compoundtag.size() == 1 && this.displayEntity instanceof Mob) {
+//            }
+//        }
+//
+//        return this.displayEntity;
 //    }
 }
